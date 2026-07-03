422706f505622a8d4b36d4a9b6d45ac90eabc8f7
özgür kavak <ozgurkvk15@gmail.com>
2026-07-03 16:45:09 +0300

    3.Ders Tamamlandı

---
+++ Python Objeleri ve Veri yapıları/ders3.py
@@ -0,0 +1,27 @@
+# ============================================
+# PYTHON DERS 3
+# Konular:
+# - Değişken oluşturma
+# - Değişken isimlendirme
+# - Farklı veri tiplerinde değişkenler
+# ============================================
+
+isim = "Özgür"
+
+yas = 22
+
+boy = 1.80
+
+ogrenci_mi = True
+
+print(isim)
+print(yas)
+print(boy)
+print(ogrenci_mi)
+
+# Değişkenlerin veri tipleri
+
+print(type(isim))
+print(type(yas))
+print(type(boy))
+print(type(ogrenci_mi))
