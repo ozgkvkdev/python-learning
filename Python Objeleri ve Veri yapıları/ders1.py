e4af5252b32291414fcc0a07b2147b9e464ea666
özgür kavak <ozgurkvk15@gmail.com>
2026-07-03 16:42:09 +0300

    1.Ders Tamamlandı

---
+++ Python Objeleri ve Veri yapıları/ders1.py
@@ -0,0 +1,36 @@
+# ============================================
+# PYTHON DERS 1
+# Konular:
+# - print() fonksiyonu
+# - Integer (int)
+# - Float (float)
+# - String (str)
+# - type() fonksiyonu
+# ============================================
+
+# print() kullanımı
+
+print("Merhaba")
+print("Yaşım :")
+print(22)
+
+print("Boyum :")
+print(1.80)
+
+print("----------------------")
+
+# Veri tiplerini öğrenelim
+
+print(type(125))
+print(type(78.9))
+print(type("Bilgisayar"))
+print(type(-56))
+print(type(0.5))
+
+print("----------------------")
+
+# int, float ve string farkı
+
+print(type(100))
+print(type(100.0))
+print(type("100"))
