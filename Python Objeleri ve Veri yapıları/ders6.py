0d58f02d98c1003788dd405657a427f60cc6e3e1
özgür kavak <ozgurkvk15@gmail.com>
2026-07-03 17:32:42 +0300

    6.Ders Tamamlandı

---
+++ Python Objeleri ve Veri yapıları/ders6.py
@@ -0,0 +1,18 @@
+"""
+pi=3.14
+r=float(input("Dairenin yarıçapını giriniz : "))
+alan=pi*(r**2)
+cevre=2*pi*r
+print("Dairenin alanı : ",alan)
+print("Dairenin çevresi :",cevre)
+
+"""
+
+pi=3.14
+r=float(input("Dairenin yarıçapını giriniz : "))
+alan=pi*(r**2)
+cevre=2*pi*r
+print("Dairenin alanı : ",alan)
+print("Dairenin çevresi :",cevre)
+
+
