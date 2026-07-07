c3eef9c5c552a06be6ee455bdb41ac3acefdaa96
özgür kavak <ozgurkvk15@gmail.com>
2026-07-07 16:21:18 +0300

    12.Ders Tamamlandı

---
+++ Python Objeleri ve Veri yapıları/ders12.py
@@ -0,0 +1,16 @@
+listem=[1,2,3]
+print(listem)
+
+alimlistesi=["araba","ev",True ,"bilgisayar"]
+print(alimlistesi)
+
+
+
+list1=["bir","iki","üç"]
+list2=["dört","beş","altı"]
+total=list1+list2
+print(total)
+
+
+
+print(len(total))
