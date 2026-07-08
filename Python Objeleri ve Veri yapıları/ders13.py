126d04bc159126f92f9c5c0cfdbb04d88dbdd247
özgür kavak <ozgurkvk15@gmail.com>
2026-07-08 15:22:05 +0300

    13.Ders Tamamlandı

---
+++ Python Objeleri ve Veri yapıları/ders13.py
@@ -0,0 +1,45 @@
+# soru 1 =  bwm , mercedes , opel , mazda elemanlarına sahip bir liste oluşturun 
+
+list1=["bmw" , "mercedes ", "opel", "mazda"]
+#print(list1)
+
+
+# soru 2 = liste kaç elemanlıdır
+# result=len(list1)
+# print(list1)
+
+# soru 3 = listenin ilk ve son elemanı nedir
+#result=list1[0]
+#print(list1)
+#result=list1[1]
+
+# soru 4 = mazda değerini toyata ile değiştirin
+#list1[-1] = 'toyota'
+#result=list1
+#print(list1)
+
+
+# soru 5 = mercedes listenin bir elemanımıdır nedir
+result="mercedes" in list1
+print(list1)
+
+
+# soru 6 =  listenin - 2 değeri nedir
+
+# soru 7 = listenin ilk 3 elemanını alın
+
+# soru 8 = listenin son iki elemanını  yerine toyata ve renault markasını ekeleyenin 
+
+# soru 9 = listenin üzerine audi ve nissan değerini ekleyin 
+
+# soru 10 = listenin son elelmaını silin 
+
+# soru 11 = liste elemanlarını tersten yazdırın 
+
+# soru 12 = asağıdaki verileri bir liste içinde saklayın 
+
+            # studentA= yiğit bilgi 2010 (70,60,70)
+            # studentB= sena turan 1999 (80,80,70)
+            # studentC=  özgür kavak (80,70,90)
+
+# soru 13 liste elemanlarını ekrana yazdırın
