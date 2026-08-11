a76d882c978c3474e384074fc72227dca8b2593b
özgür kavak <ozgurkvk15@gmail.com>
2026-08-11 16:53:16 +0300

    Dosya Yönetimi #3

---
+++ Pythonda Dosya Yönetimi/files3.py
@@ -0,0 +1,12 @@
+# # dosya okuma fonksiyonları
+
+# with open("ornek.txt","r", encoding="utf-8") as file:   # dosya okundu with open() fonksiyonu ile dosya açılır ve dosya kapatılır. 
+#     content = file.read(10) # dosya içeriğini tek seferde okur ve bir string olarak döndürür.
+#     file.seek(0) # dosya imlecini başa alır
+#     print(content) # dosya içeriğini ekrana yazdır
+#     print(file.tell()) # dosya imlecinin konumunu verir
+
+# # with ne işe yarar ? 
+# # with open() fonksiyonu ile dosya açılır ve dosya kapatılır.
+
+
