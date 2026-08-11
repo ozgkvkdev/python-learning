2e034c3d858254b3266e508dd5cba4921462dc71
özgür kavak <ozgurkvk15@gmail.com>
2026-08-11 16:40:37 +0300

    Dosya Yönetimi #2

--- Pythonda Dosya Yönetimi/files2.py
+++ Pythonda Dosya Yönetimi/files2.py
@@ -1,5 +1,23 @@
-import time
+# # dosya okuma işlemleri için read() fonksiyonu kullanılır. 
 
-for i in range(20):
-    print("Hello, World!")
-    time.sleep(1)
+# file = open("ornek.txt","r", encoding="utf-8")   # dosya okundu
+
+# # for döngüsü ile dosya içeriğini satır satır ekrana yazdırmak için dosya nesnesi üzerinde iterasyon yapılabilir.
+# for i in file:
+#     print(i,end="") # dosya içeriğini ekrana yazdır end parametresi ile satır sonuna ekleme yapılmaz
+# file.close() # dosya kapatıldı  
+
+# content = file.read() # dosya içeriğini tek seferde okur ve bir string olarak döndürür.
+# print(content) # dosya içeriğini ekrana yazdır
+
+
+# from click import File
+
+
+# content = File.readline() # type: ignore # dosya içeriğini satır satır okur ve bir liste olarak döndürür.
+# print(content) # dosya içeriğini ekrana yazdır
+
+
+# liste = file.readlines() # dosya içeriğini satır satır okur ve bir liste olarak döndürür.
+# print(liste) # dosya içeriğini ekrana yazdır
+
